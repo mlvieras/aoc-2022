de9aa46333ea672908d7dc5cf9f1ca4502f58f7e
Matías Lorenzo <mlvieras@gmail.com>
2022-12-28 19:57:36 -0300

    WIP

--- day14/1.js
+++ day14/1.js
@@ -9,13 +9,16 @@ const main = async () => {
   const data = fs.readFileSync('./input.txt', 'utf-8')
     .split('\n')
     .slice(0, -1)
-    .map(r => r.split(POINT_SPLIT).map(p => p.split(COORDINATE_SPLIT)))
+    .map(r => r.split(POINT_SPLIT).map(p => p.split(COORDINATE_SPLIT).map(n => parseInt(n, 10))))
     .reduce((result, pointList) => (
       [...result, ...pointList.map((p, index, list) => index !== list.length - 1 ? [p, list[index + 1]] : null).filter(Boolean)]
     ), [])
 
 
   const maxYCoordinate = Math.max(...data.map(r => Math.max(r[0][1], r[1][1])))
+  const minYCoordinate = Math.min(...data.map(r => Math.min(r[0][1], r[1][1])))
+  const maxXCoordinate = Math.max(...data.map(r => Math.max(r[0][0], r[1][0])))
+  const minXCoordinate = Math.min(...data.map(r => Math.min(r[0][0], r[1][0])))
   let blockDict = data.reduce((acc, [firstPoint, secondPoint]) => {
     const offsetX = firstPoint[0] - secondPoint[0]
     const offsetY = firstPoint[1] - secondPoint[1]
@@ -29,24 +32,53 @@ const main = async () => {
       if (offsetY) {
         y = Math.min(firstPoint[1], secondPoint[1]) + i
       }
-      acc[`${x}-${y}`] = true
+      acc[x] = acc[x] || {}
+      acc[x][y] = true
     })
     return acc
   }, {})
+
+  const markPosition = (x, y) => {
+    blockDict[x] = blockDict[x] || {}
+    blockDict[x][y] = true
+  }
 
   let abyssReached = true
   let sandIndex = 0
   while(!abyssReached) {
     sandIndex + 1
 
-    let currentPosition = SAND_DROP_POINT
-    while (currentPosition[1] <= maxYCoordinate) {
-
+    let [currentX, currentY] = SAND_DROP_POINT
+    let restReached = false
+    while (currentY <= maxYCoordinate && !restReached) {
+      console.log(currentX, currentY)
+      if (!blockDict[currentX][currentY + 1]) {
+        currentY = currentY + 1
+        continue
+      }
+      if (!blockDict[currentX - 1][currentY + 1]) {
+        currentX = currentX - 1
+        currentY = currentY + 1
+        continue
+      }
+      if (!blockDict[currentX + 1][currentY + 1]) {
+        currentX = currentX + 1
+        currentY = currentY + 1
+        continue
+      }
+      restReached = true
+      markPosition(currentX, currentY)
     }
+    abyssReached = !restReached
   }
 
-  console.log(blockDict)
+  console.log(minXCoordinate, maxXCoordinate, minYCoordinate, maxYCoordinate)
+  console.log(sandIndex)
 
+  const testString = Array.from({length: maxXCoordinate - minXCoordinate}).map((_, i) => (
+    Array.from({length: maxYCoordinate - minYCoordinate}).map((__, j) => (blockDict[i + minXCoordinate] && blockDict[i + minXCoordinate][j + minYCoordinate]) ? '#' : '.').join('')
+  )).join('\n')
+  fs.writeFileSync('test.txt', testString)
 }
 
 main();
